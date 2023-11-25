922b788676810b2f8767ed5752591ae684882b3b
Nicholas Talton <91391680+Nick-Talton@users.noreply.github.com>
2023-11-24 21:58:07 -0500

    adding example second page and database connection is more secure for refreshing data

--- app/test.py
+++ app/test.py
@@ -3,25 +3,27 @@ import pymysql
 
 app = Flask(__name__)
 
-
-db = pymysql.connect(host='mysql01.cs.virginia.edu',
-                     user='nrt3xs',
-                     password='UVa107CS4750!',
-                     database='nrt3xs',
-                     cursorclass=pymysql.cursors.DictCursor)
-
-# Create a cursor to interact with the database
-cursor = db.cursor()
+def get_db():
+    return pymysql.connect(host='mysql01.cs.virginia.edu',
+                           user='nrt3xs',
+                           password='UVa107CS4750!',
+                           database='nrt3xs',
+                           cursorclass=pymysql.cursors.DictCursor)
 
 @app.route('/')
 def display_accounts():
+    db = get_db()
+    cursor = db.cursor()
     query = "SELECT * FROM account"
     cursor.execute(query)
     accounts = cursor.fetchall()
-
-    #db.close()
+    db.close()
 
     return render_template('accounts.html', accounts=accounts)
+
+@app.route('/second_page')
+def second_page():
+    return render_template('second_page.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
