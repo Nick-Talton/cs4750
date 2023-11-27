8779e424cb978d87835b6a139cbd89ee43e1b723
evelyn-tse <eyt7ph@virginia.edu>
2023-11-27 11:07:27 -0500

    Remove Appointments

--- app/app.py
+++ app/app.py
@@ -5,7 +5,6 @@ from flask import Flask, render_template, request, redirect, url_for
 from homepage import homepage
 from loginpage import loginpage
 from signuppage import signuppage
-from appointmentpages import appointmentpages
 from cartpage import cartpage
 from postspages import postspages
 from profilepage import profilepage
@@ -24,7 +23,6 @@ def get_db():
 app.register_blueprint(homepage) # /
 app.register_blueprint(loginpage) # /login
 app.register_blueprint(signuppage) # /signup
-app.register_blueprint(appointmentpages) # /appointment and /appointment/confirmation/<name>/<date>/<time>
 app.register_blueprint(cartpage) # /cart
 app.register_blueprint(postspages) # /post/create and /post/<int:id>
 app.register_blueprint(profilepage) # /profile/<user>
