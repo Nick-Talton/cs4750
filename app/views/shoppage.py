e3fe93858de3b8e82ffa8bab7e4180a86e0fde60
Nicholas Talton <91391680+Nick-Talton@users.noreply.github.com>
2023-12-03 05:30:49 -0500

    filtering pain

--- app/views/shoppage.py
+++ app/views/shoppage.py
@@ -1,42 +1,9 @@
 # searchpages.py
-from flask import Blueprint, render_template, request, session
+from flask import Blueprint, render_template, request, session, jsonify
 from jinja2 import TemplateNotFound
 import pymysql
 
 shoppage = Blueprint('shop', __name__, template_folder='templates')
-
-# A list of users to display on the search results page
-# users = [
-#     {
-#         "username": "alice",
-#         "bio": "I love Flask and Python."
-#     },
-#     {
-#         "username": "bob",
-#         "bio": "I'm a beginner in web development."
-#     },
-#     {
-#         "username": "charlie",
-#         "bio": "I'm a fan of open source software."
-#     }
-# ]
-
-# @shop.route("/shop", methods=["GET", "POST"])
-# def search():
-#     # If the request method is POST, get the query and filter the users list
-#     if request.method == "POST":
-#         query = request.form.get("query")
-#         # You can use your own logic to search for users here
-#         results = [user for user in users if query.lower() in user["username"].lower()]
-#         # Render the results.html template with the query and results variables
-#         return render_template("results.html", query=query, results=results)
-#     # If the request method is GET, render the search.html template
-#     return render_template("shop.html", title='Shop')
-
-# @shoppage.route("/shop", methods=["GET", "POST"])
-# def shop():
-#     # Render the index.html template with the posts variable
-#     return render_template("shop.html", title='Shop', posts=posts)
 
 
 def get_db():
@@ -51,17 +18,49 @@ def get_db():
 @shoppage.route("/shop", methods=["GET", "POST"])
 def shop():
     if 'email' in session:
-        # print("user logged in")
-        # print("session:", session)
-        # logged_in_user = session['email']
         first_name = session['first_name']
         session_user = session['user']
-        with get_db() as connection:
+
+        if request.method == 'POST':
+            try:
+                # Retrieve the selected filters from the request
+                selected_filters = request.json.get("filters", [])
+
+                # Construct the WHERE clause based on the selected filters
+                if selected_filters:
+                    # Construct the WHERE clause for the selected filters
+                    updated_filters = []
+                    for filter in selected_filters:
+                        updated_filters.append("'" + filter + "'")
+                    where_clause = f"animal_class IN ({', '.join([filter for filter in updated_filters])})"
+                    
+                    # Query to filter the data based on the selected filters
+                    query = f"SELECT pet_id, breed, name, price, age FROM Pets NATURAL JOIN Breeds NATURAL JOIN Birthdays WHERE {where_clause};"
+                else:
+                    # Query without filtering if no filters are selected
+                    query = "SELECT pet_id, breed, name, price, age FROM Pets NATURAL JOIN Breeds NATURAL JOIN Birthdays;"
+
+                with get_db() as connection:
                     with connection.cursor() as cursor:
-                        query = "SELECT pet_id, breed, name, price, age FROM Pets NATURAL JOIN Breeds NATURAL JOIN Birthdays;"
                         cursor.execute(query)
-                        posts = cursor.fetchall()
-        return render_template('shop.html', title='Shop', username=first_name, posts=posts)
+                        filtered_posts = cursor.fetchall()
+
+                return jsonify({"html": render_template('shop.html', title='Shop', username=first_name, posts=filtered_posts)})
+
+            except Exception as e:
+                print(e)
+                return jsonify({"error": "An error occurred during filtering."})
+
+        else:
+            # Handle GET request without applying any filter
+            with get_db() as connection:
+                with connection.cursor() as cursor:
+                    query = "SELECT pet_id, breed, name, price, age FROM Pets NATURAL JOIN Breeds NATURAL JOIN Birthdays;"
+                    cursor.execute(query)
+                    posts = cursor.fetchall()
+
+            return render_template('shop.html', title='Shop', username=first_name, posts=posts)
     else:
-        # print("no user logged in")
         return render_template('index.html', title='Home')
+
+    
