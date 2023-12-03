cb20c1664787a212408418d9e7a0301b890b9140
Nicholas Talton <91391680+Nick-Talton@users.noreply.github.com>
2023-12-03 01:35:17 -0500

    doing grant statements in profile page

--- app/views/profilepage.py
+++ app/views/profilepage.py
@@ -1,5 +1,5 @@
 # profilepage.py
-from flask import Blueprint, render_template, session, request
+from flask import Blueprint, render_template, session, request, redirect, url_for
 from jinja2 import TemplateNotFound
 import pymysql, bcrypt
 
@@ -40,6 +40,7 @@ def profile():
                 new_password = request.form.get("new_password")
                 confirm_new_password = request.form.get("confirm_new_password")
 
+
                 if new_password == current_password and (bcrypt.checkpw(current_password.encode('utf-8'), session_user['password'].encode('utf-8'))):
                     return render_template('profile.html', title='Profile', password_error='New Password Cannot be Current Password.', username=first_name, user=session_user)
 
@@ -54,7 +55,6 @@ def profile():
                         cursor.execute(query, (logged_in_user,first_name,))
                         user = cursor.fetchone()
 
-                        # Ensure user is the user we want
                         if user and (bcrypt.checkpw(current_password.encode('utf-8'), user['password'].encode('utf-8'))):
                             query = "UPDATE Users SET password=%s WHERE email=%s AND first_name=%s"
                             cursor.execute(query, (new_hashed_password, logged_in_user,first_name,))
@@ -73,10 +73,53 @@ def profile():
                 return render_template('profile.html', title='Profile', username=first_name, user=user)
 
         else:
-            # print("no user logged in")
             return render_template('index.html', title='Home')
     except Exception as e:
         print(e)
         return render_template('profile.html', title='Profile', error='Whoops... something happened. Please login again.')
             
-                            
+@profilepage.route('/grant_admin_privileges', methods=['GET', 'POST'])
+def grant_admin_privileges():
+    try:
+        logged_in_user = session['email']
+        first_name = session['first_name']
+        session_user = session['user']
+        # Check if user is logged in and request method is POST
+        if 'email' in session and request.method == 'POST':
+            # print("in here")
+
+            # Check if the provided admin password is correct
+            admin_password = "admin_password"  # Replace with your actual admin password
+            user_admin_password = request.form.get("admin")
+
+            if admin_password == user_admin_password:
+                # print("written correctly")
+                # If the admin password is correct, execute the GRANT statement
+                with get_db() as connection:
+                    with connection.cursor() as cursor:
+                        # Modify the query to grant the necessary privileges
+                        # print("at query statement")
+                        query = "GRANT ALL PRIVILEGES ON nrt3xs TO 'nrt3xs_a'@'%';"
+                        cursor.execute(query)
+                        connection.commit()
+                # print("before return")
+                # Redirect the user to the profile page or a success page
+                return render_template('profile.html', title='Profile', admin_error='Admin Access Granted.', username=first_name, user=session_user)
+                # return redirect(url_for('profilepage.profile', username=first_name, user=session_user))
+
+            else:
+                return render_template('profile.html', title='Profile', admin_error='Incorrect admin password.', username=first_name, user=session_user)
+
+        else:
+            with get_db() as connection:
+                with connection.cursor() as cursor:
+                    query = "SELECT * FROM Users WHERE email=%s AND first_name=%s"
+                    cursor.execute(query, (logged_in_user,first_name,))
+                    user = cursor.fetchone()
+
+            return render_template('profile.html', title='Profile', username=first_name, user=user)
+
+    # Handle exceptions
+    except Exception as e:
+        print(e)
+        return render_template('profile.html', title='Profile', error='Whoops... something happened. Please try again.', username=first_name)
